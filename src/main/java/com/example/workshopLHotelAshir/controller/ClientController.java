ef7e171fbd0b9d3d8612ecbc4b784fe1606f72f7
Sofía Millán <sofiaisabellamillan16@gmail.com>
2023-04-24 17:30:32 -0500

    Refactoring controller

--- src/main/java/com/example/workshopLHotelAshir/controller/ClientController.java
+++ src/main/java/com/example/workshopLHotelAshir/controller/ClientController.java
@@ -18,7 +18,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/api/v1")
 public class ClientController {
 
-    private ClientService clientService;
+    private final ClientService clientService;
 
     @Autowired
     public ClientController(ClientService clientService) {
